962040ed359a0fb6c1c288d28232f2bda7c7fadd
Rizarid <rizarid@mail.ru>
2021-05-29 11:53:45 +0500

    Init

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const { Template } = require('webpack');
+const autoprefixer = require('autoprefixer')
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: "all" 
+        }
+    }
+
+    if(isProd){
+        config.minimizer = [
+            new OptimizeCssAssetsPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+
+    return config
+}
+
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    optimization: optimization(),
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: 'src/index.pug',
+            inject: 'body',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthesh].css'
+        }),
+        new CleanWebpackPlugin(),
+        autoprefixer
+    ],
+    
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.pug$/,
+                use: 'pug-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpeg|svg|gif)$/,
+                use: 'file-loader'
+            },
+            {
+                test:  /\.(ttf|woff|svg)/,
+                use: 'file-loader'
+            }
+        ]
+    }
+
+    
+}
