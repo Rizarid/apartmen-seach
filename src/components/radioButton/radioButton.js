083655042cfde9de93d3985885577d77ab3d1380
Rizarid <rizarid@mail.ru>
2021-06-13 23:48:19 +0500

    RadioButton code refactoring

--- src/components/radioButton/radioButton.js
+++ src/components/radioButton/radioButton.js
@@ -1,1 +1,13 @@
 import './radioButton.sass'
+import * as $ from 'jquery'
+
+const radioButtons = $(".radioButton__item");
+
+if(radioButtons.length){
+  radioButtons.click(radioButtonClick)
+}
+
+function radioButtonClick(){
+  $(this).parent().children().removeClass("checked")
+  $(this).toggleClass("checked")
+}
