cdd8e21cb3e736ed4b1ffebdfecba3dbcc7b0cf6
Rizarid <rizarid@mail.ru>
2021-07-15 19:41:36 +0500

    radio-button component code refactoring

---
+++ src/components/radio-button/radio-button.js
@@ -0,0 +1,18 @@
+import * as $ from "jquery";
+
+import "./radio-button.sass";
+
+
+{
+  const $radioButtons = $(".js-radio-button__item");
+
+  if($radioButtons.length){
+    $radioButtons.click(switchButton);
+  }
+
+  function switchButton(){
+
+    $(this).parent().children().removeClass("radio-button__item_checked");
+    $(this).addClass("radio-button__item_checked");
+  }
+}
