de55f4af811508b8de2e2990d82f79c90a5ce7ed
Rizarid <rizarid@mail.ru>
2021-07-15 18:49:36 +0500

    Button component code refactoring

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,6 +1,2 @@
-import './button.sass'
-import './_submit/button_submit.sass'
-import './_transparent/button_transparent.sass'
-import './_big/button_big.sass'
-import './__text/button__text.sass'
-import './_minimal/button_minimal.sass'
+import "./button.sass";
+
