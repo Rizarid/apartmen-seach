6e2fcfcbec68d9ebdd35f35613a9a19c37bf280e
Rizarid <rizarid@mail.ru>
2021-07-15 19:38:30 +0500

    masked-text-field component code refactoring

---
+++ src/components/masked-text-field/masked-text-field.js
@@ -0,0 +1,14 @@
+import * as $ from "jquery";
+
+import Inputmask from "inputmask"; 
+
+import "../text-field/text-field.js";
+import "./masked-text-field.sass";
+
+{
+
+  const maskedFields = $(".js-masked-text-field .text-field__field");
+  const inputmask = new Inputmask({ alias: "datetime", inputFormat: "dd.mm.yyyy", placeholder: "ДД.ММ.ГГГГ"});
+  inputmask.mask(maskedFields);
+
+}
