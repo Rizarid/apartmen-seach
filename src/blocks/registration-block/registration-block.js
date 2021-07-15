f24844665d13cef9d34dea2f2c3a6cbdbf492684
Rizarid <rizarid@mail.ru>
2021-07-15 21:22:01 +0500

    registration-block code refactoring

---
+++ src/blocks/registration-block/registration-block.js
@@ -0,0 +1,7 @@
+import "../../components/text-field/text-field.js";
+import "../../components/radio-button/radio-button.js";
+import "../../components/masked-text-field/masked-text-field.js";
+import "../../components/toggle/toggle.js";
+import "../../components/button/button.js";
+import "./registration-block.sass";
+
