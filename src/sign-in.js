aec1012a24a5cac4691f7b0db206d396f591835c
Rizarid <rizarid@mail.ru>
2021-07-15 23:24:50 +0500

    Added sugn-in page

---
+++ src/sign-in.js
@@ -0,0 +1,7 @@
+import "./styles/sign-in.sass";
+import "./fonts/fonts.css";
+import "./fonts/scss/_path.scss";
+import "./components/slider-of-background/slider-of-background.js";
+import "./blocks/sign-in-block/sign-in-block.js"
+import "./blocks/header/header.js";
+import "./blocks/footer/footer.js";
