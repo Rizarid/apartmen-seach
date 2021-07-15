7476653a264330308299b24b9b65501f0939073f
Rizarid <rizarid@mail.ru>
2021-07-15 22:08:25 +0500

    Added registration page

---
+++ src/registration.js
@@ -0,0 +1,7 @@
+import "./components/slider-of-background/slider-of-background.js";
+import "./blocks/registration-block/registration-block.js";
+import "./blocks/header/header.js";
+import "./blocks/footer/footer.js";
+import "./fonts/fonts.css";
+import "./fonts/scss/_path.scss";
+import "./styles/registration.sass";
