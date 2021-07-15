b43c81674f421a7ee8517c94e3fba567fa7a7af4
Rizarid <rizarid@mail.ru>
2021-07-15 22:05:48 +0500

    Added landing page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import * as $ from 'jquery'
-import './fonts/fonts.css'
-import './styles/style.sass'
-import './components/button/button.js'
-import './components/toggle/toggle.js'
-import './components/textField/textField.js'
-//import './components/dropdown/dropdown.js'
-//import './components/slider/slider.js'
-//import './components/filterDateDropdown/filterDateDropdown.js'
-import './components/subscriptionField/subscriptionField.js'
-import './components/expandableCheckboxList/expandableCheckboxList.js'
+import "./fonts/fonts.css";
+import "./fonts/scss/_path.scss";
+import "./styles/style.sass";
+import "./blocks/header/header.js";
+import "./blocks/search-block/search-block.js";
+import "./blocks/footer/footer.js";
+import "./components/slider-of-background/slider-of-background.js";
