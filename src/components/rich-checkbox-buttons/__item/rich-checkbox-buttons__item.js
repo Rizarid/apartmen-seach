07a2664dd535a7c30052ba1a0a2abba522ba1efc
Rizarid <rizarid@mail.ru>
2021-07-15 19:47:08 +0500

    rich-checkbox-buttons component code refactoring

---
+++ src/components/rich-checkbox-buttons/__item/rich-checkbox-buttons__item.js
@@ -0,0 +1,2 @@
+import "../../checkbox/checkbox.js";
+import "./rich-checkbox-buttons__item.sass";
---
+++ src/components/rich-checkbox-buttons/rich-checkbox-buttons.js
@@ -0,0 +1,2 @@
+import "./rich-checkbox-buttons.sass";
+import "./__item/rich-checkbox-buttons__item.js";
