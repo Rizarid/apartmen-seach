353ff3f7b037a76e7cd05e2559dd4150756d7b99
Rizarid <rizarid@mail.ru>
2021-07-15 21:30:10 +0500

    Added information-about-room block

---
+++ src/blocks/information-about-room/information-about-room.js
@@ -0,0 +1,5 @@
+import "../../components/information-block/information-block.js";
+import "../../components/round-chart/round-chart.js";
+import "../../components/reviews/reviews.js";
+import "../../components/bullet-list/bullet-list.js";
+import "./information-about-room.sass";
