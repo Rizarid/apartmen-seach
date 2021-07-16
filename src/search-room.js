8145417f1fb8750f3c358e2c44208a160ac9d99e
Rizarid <rizarid@mail.ru>
2021-07-17 04:03:28 +0500

    Change room-search pages adaptive styles

--- src/search-room.js
+++ src/search-room.js
@@ -1,6 +1,7 @@
 import "./fonts/fonts.css";
 import "./fonts/scss/_path.scss";
 import "./styles/search-room.sass";
+import "./scripts/create-element.js"
 import "./blocks/header/header.js";
 import "./blocks/room-card-block/room-card-block.js";
 import "./blocks/footer/footer.js";
@@ -22,8 +23,10 @@ import { ListConvenience } from "./components/dropdown/__list/dropdown__list";
   searchRoom.convenience = new DropdownClass({parentSelector: "convenience-filter", items: {"спальни":  2, "кровати": 2 , "ванные комнаты": 0}, getListClass: (items) => {return new ListConvenience(items)}});
   searchRoom.priceFilter = new Slider({parentSelector: "price-filter", min: 0, max:16000, initValues: ["5000", "10000"]});
 
-  searchRoom.filters = document.querySelector(".filters");
-  searchRoom.filtersButton = document.querySelector(".filters__show-button");
+  searchRoom.filters = document.querySelector(".js-filters");
+  searchRoom.navigation = document.querySelector(".js-header__navigation");
+  searchRoom.filtersButton = createElement("div", "filters-button");
+  searchRoom.navigation.insertAdjacentElement("afterEnd", searchRoom.filtersButton);
 
   searchRoom.showFilters = () => {
     searchRoom.filters.classList.toggle("show");
