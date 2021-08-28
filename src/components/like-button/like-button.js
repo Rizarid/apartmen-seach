aac1155a81f7529951e0aabd92a7baa42e56691e
Rizarid <rizarid@mail.ru>
2021-08-29 02:08:13 +0500

    Refactoring of like-button component

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -5,22 +5,16 @@ import "./like-button.sass";
 
 {
   const $likeButtons = $(".js-like-button");
-
-  if($likeButtons.length){
-    $likeButtons.click(handleLikeButtonClick);
-  }
+  if($likeButtons.length) $likeButtons.click(handleLikeButtonClick);
 
   function handleLikeButtonClick(){
-
     $(this).toggleClass("like-button_liked");
+    let text;
 
-    let text
+    $(this).hasClass("like-button_liked") 
+      ? text = Number($(this).children(".like-button__quantity").text()) + 1 
+      : text = Number($(this).children(".like-button__quantity").text()) -1;
 
-    ($(this).hasClass("like-button_liked")) ?
-      text = Number($(this).children(".like-button__quantity").text()) + 1 :
-      text = Number($(this).children(".like-button__quantity").text()) -1;
-
-    $(this).children(".like-button__quantity").html(String(text));
-
+    $(this).children(".like-button__quantity").html(text.toString());
   }
 }
