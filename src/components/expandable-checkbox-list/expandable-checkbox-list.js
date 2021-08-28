7493625d14fbb9270bb55c7fa8a25d31ef850e89
Rizarid <rizarid@mail.ru>
2021-08-29 00:40:09 +0500

    Refactoring of expandable-checkbox-list component

--- src/components/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/components/expandable-checkbox-list/expandable-checkbox-list.js
@@ -4,13 +4,10 @@ import "./expandable-checkbox-list.sass";
 import "./__item/expandable-checkbox-list__item.js";
 
 {
-  const expandableCheckboxList = $(".js-expandable-checkbox-list__conteiner-of-title")
-    
-  if(expandableCheckboxList.length){
-    expandableCheckboxList.click(showExpandableCheckboxList)
-  }
+  const $expandableCheckboxList = $(".js-expandable-checkbox-list__container-of-title");
+  if($expandableCheckboxList.length) $expandableCheckboxList.click(showExpandableCheckboxList);
 
   function showExpandableCheckboxList(){
-    $(this).toggleClass("expandable-checkbox-list__conteiner-of-title_visible")
+    $(this).toggleClass("expandable-checkbox-list__container-of-title_visible");
   }
 }
