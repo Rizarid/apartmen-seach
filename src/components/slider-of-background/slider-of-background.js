274658fc0ab9b2bd89431858a9f558d06377fdf1
Rizarid <rizarid@mail.ru>
2021-08-29 18:25:40 +0500

    Refactoring of slider-of-background component

--- src/components/slider-of-background/slider-of-background.js
+++ src/components/slider-of-background/slider-of-background.js
@@ -2,17 +2,13 @@ import "./slider-of-background.sass";
 
 {
   const slider = document.querySelector(".js-slider-of-background");
-
   const slides = slider.children;
-
   let i = 0;
-
   setInterval(nextSlide, 10000);
 
   function nextSlide(){
     slides[i].classList.remove("slider-of-background__image_active");
-    if (i + 1 < slides.length) i++
-    else i = 0
+    i = (i + 1 < slides.length) ?  i + 1 :  0;
     slides[i].classList.add("slider-of-background__image_active");
   }
 }
