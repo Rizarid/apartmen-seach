fcc3c6d9cb447c9592ccc6d327cd2adc9ea5f941
Rizarid <rizarid@mail.ru>
2021-08-29 21:53:27 +0500

    Refactoring of search-block

--- src/blocks/search-block/search-block.js
+++ src/blocks/search-block/search-block.js
@@ -6,6 +6,10 @@ import {ListGuests} from "../../components/dropdown/__list/dropdown__list.js";
 import "./search-block.sass";
 
 {
-  const dateDropdown = new DateDropdown({parentSelector: "date-selection"});
-  const dropdown = new DropdownClass({parentSelector: "guests-quantity", items: {"взрослые": 0, "дети": 0, "младенцы": 0}, getListClass: (items) => {return new ListGuests(items)}}); 
+  const dateDropdown = new DateDropdown({ parentSelector: "date-selection" });
+  const dropdown = new DropdownClass({
+    parentSelector: "guests-quantity", 
+    items: { "взрослые": 0, "дети": 0, "младенцы": 0 }, 
+    getListClass: (items) => new ListGuests(items)
+  }); 
 }
