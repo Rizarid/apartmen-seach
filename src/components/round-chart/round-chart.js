1d5b0be664dce4a7be37722f37927a4cc6853d09
Rizarid <rizarid@mail.ru>
2021-09-02 12:45:59 +0500

    Refactoring round-chart component

--- src/components/round-chart/round-chart.js
+++ src/components/round-chart/round-chart.js
@@ -1,23 +1,17 @@
 import './round-chart.sass';
 
-const getRoundChart = function (data = []) {
-  const chartData = {};
-  chartData.data = data;
+const getRoundChart = function (options = []) {
+  const votes = options;
+  const sum = votes.reduce((summa, current) => summa + current, 0);
+  const dash = votes.map((item) => ((item > 0) ? Math.abs((item / sum) * 100 - 0.5) : 0));
+  const dashoffset = [];
 
-  chartData.sum = chartData.data.reduce((sum, current) => sum + current, 0);
-  chartData.dash = chartData.data.map((item) => (
-    (item > 0) ? Math.abs((item / chartData.sum) * 100 - 0.5) : 0
-  ));
-
-  chartData.dashoffset = [];
-
-  for (let i = 0; i < chartData.dash.length; i += 1) {
+  for (let i = 0; i < dash.length; i += 1) {
     if (i === 0) {
-      chartData.dashoffset.push(25);
+      dashoffset.push(25);
       continue;
     }
-
-    chartData.dashoffset.push(100 - chartData.dash[i - 1] + chartData.dashoffset[i - 1] - 0.5);
+    dashoffset.push(100 - dash[i - 1] + dashoffset[i - 1] - 0.5);
   }
 
   const chart = `
@@ -25,13 +19,13 @@ const getRoundChart = function (data = []) {
     <figure class='chart__content'>
       <svg width='100%' height='100%' viewBox='0 0 34 34' class='chart__donut'>
         <circle class='donut-hole' cx='17' cy='17' r='15.91549430918954' fill='#fff'></circle>
-        <circle class='donut-segment' cx='17' cy='17' r='15.91549430918954' fill='transparent' stroke='#909090' stroke-width='1' stroke-dasharray='${chartData.dash[0]} ${100 - chartData.dash[0]}' stroke-dashoffset='${chartData.dashoffset[0]}'></circle>
-        <circle class='donut-segment' cx='17' cy='17' r='15.91549430918954' fill='transparent' stroke='#BC9CFF' stroke-width='1' stroke-dasharray='${chartData.dash[1]} ${100 - chartData.dash[1]}' stroke-dashoffset='${chartData.dashoffset[1]}'></circle>
-        <circle class='donut-segment' cx='17' cy='17' r='15.91549430918954' fill='transparent' stroke='#6FCF97' stroke-width='1' stroke-dasharray='${chartData.dash[2]} ${100 - chartData.dash[2]}' stroke-dashoffset='${chartData.dashoffset[2]}'></circle>
-        <circle class='donut-segment' cx='17' cy='17' r='15.91549430918954' fill='transparent' stroke='#FFE39C' stroke-width='1' stroke-dasharray='${chartData.dash[3]} ${100 - chartData.dash[3]}' stroke-dashoffset='${chartData.dashoffset[3]}'></circle>
+        <circle class='donut-segment' cx='17' cy='17' r='15.91549430918954' fill='transparent' stroke='#909090' stroke-width='1' stroke-dasharray='${dash[0]} ${100 - dash[0]}' stroke-dashoffset='${dashoffset[0]}'></circle>
+        <circle class='donut-segment' cx='17' cy='17' r='15.91549430918954' fill='transparent' stroke='#BC9CFF' stroke-width='1' stroke-dasharray='${dash[1]} ${100 - dash[1]}' stroke-dashoffset='${dashoffset[1]}'></circle>
+        <circle class='donut-segment' cx='17' cy='17' r='15.91549430918954' fill='transparent' stroke='#6FCF97' stroke-width='1' stroke-dasharray='${dash[2]} ${100 - dash[2]}' stroke-dashoffset='${dashoffset[2]}'></circle>
+        <circle class='donut-segment' cx='17' cy='17' r='15.91549430918954' fill='transparent' stroke='#FFE39C' stroke-width='1' stroke-dasharray='${dash[3]} ${100 - dash[3]}' stroke-dashoffset='${dashoffset[3]}'></circle>
         <g class='chart__text'>
             <text x='50%' y='50%' class='chart__number'>
-            ${chartData.sum}
+            ${sum}
             </text>
             <text x='50%' y='50%' class='chart__label'>
             голосов
