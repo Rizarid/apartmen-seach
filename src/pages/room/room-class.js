55722fd72ef44b1584b1799de0b9e24eddee94a0
Rizarid <rizarid@mail.ru>
2021-09-08 01:11:10 +0500

    Added InformationAboutRoom to room-class

--- src/pages/room/room-class.js
+++ src/pages/room/room-class.js
@@ -1,17 +1,26 @@
-import { getRoundChart } from '@/components/round-chart/round-chart';
+import { InformationAboutRoom } from '@/blocks/information-about-room/information-about-room';
 import { Booking } from '@/blocks/booking-block/booking-block';
 
 class Room {
   constructor() {
-    this._booking = new Booking({
-      roomPrice: 9990, discount: 2179, servicesTotal: 0, additionalServicesPrice: 300,
-    });
-
-    this._chart = this._getChart();
-    this._chart.innerHTML = getRoundChart([0, 65, 65, 130]);
+    this._body = this._getBody();
+    this._booking = this._getBooking();
+    this._information = this._getInformation();
   }
 
-  _getChart = () => document.querySelector('.js-round-chart__content');
+  _getBody = () => document.querySelector('.room');
+
+  _getBooking = () => {
+    const target = this._body.querySelector('.js-booking');
+    return new Booking({
+      target, roomPrice: 9990, discount: 2179, servicesTotal: 0, additionalServicesPrice: 300,
+    });
+  }
+
+  _getInformation = () => {
+    const target = this._body.querySelector('.information-about-room');
+    return new InformationAboutRoom({ target, votes: [0, 65, 65, 130] });
+  }
 }
 
 export { Room };
