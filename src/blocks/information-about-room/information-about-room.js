e1098c6192ae008431a63d7f8dc8aa31222df5b0
Rizarid <rizarid@mail.ru>
2021-09-08 01:02:52 +0500

    Added InformationAboutRoom class

--- src/blocks/information-about-room/information-about-room.js
+++ src/blocks/information-about-room/information-about-room.js
@@ -1,4 +1,27 @@
 import '../../components/information-block/information-block';
-import '../../components/reviews/reviews';
+import { Reviews } from '../../components/reviews/reviews';
+import { RoundChart } from '../../components/round-chart/round-chart';
 import '../../components/bullet-list/bullet-list';
 import './information-about-room.sass';
+
+class InformationAboutRoom {
+  constructor(options) {
+    const { target, votes } = options;
+    this._body = target;
+    this._reviews = this._getReviews(); 
+    this._roundChart = new RoundChart(votes);
+    this._appendChart();
+  }
+
+  _getReviews = () => {
+    const reviews = this._body.querySelector('.js-reviews');
+    return new Reviews(reviews);
+  };
+
+  _appendChart = () => {
+    const chart = this._body.querySelector('.js-round-chart__content');
+    chart.innerHTML = this._roundChart.getChart()
+  }
+}
+
+export { InformationAboutRoom };
