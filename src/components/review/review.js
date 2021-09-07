f9249bad3eb94ff75c4285cb74ac6137bbc56ca4
Rizarid <rizarid@mail.ru>
2021-09-08 00:57:00 +0500

    Changed review and reviews components

--- src/components/review/review.js
+++ src/components/review/review.js
@@ -1,2 +1,16 @@
-import '../like-button/like-button';
+import { LikeButton } from '../like-button/like-button';
 import './review.sass';
+
+class Review {
+  constructor(target) {
+    this._body = target;
+    this._likeButton = this._getLikeButton();
+  }
+
+  _getLikeButton = () => {
+    const likeButtonTarget = this._body.querySelector('.js-like-button');
+    return new LikeButton(likeButtonTarget);
+  }
+}
+
+export { Review };
--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -1,2 +1,16 @@
-import '../review/review';
+import { Review } from '../review/review';
 import './reviews.sass';
+
+class Reviews {
+  constructor(target) {
+    this._body = target;
+    this._items = this._getItems();
+  }
+
+  _getItems = () => {
+    const items = Array.prototype.slice.call(this._body.querySelectorAll('.js-review'));
+    return items.map((item) => new Review(item));
+  }
+}
+
+export { Reviews };
