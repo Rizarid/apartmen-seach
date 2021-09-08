b45d803a985959f002512f0eb90c8cc8e17b9491
Rizarid <rizarid@mail.ru>
2021-09-08 18:27:16 +0500

    Refactoring registration-block

--- src/blocks/registration-block/registration-block.js
+++ src/blocks/registration-block/registration-block.js
@@ -1,6 +1,33 @@
 import '../../components/text-field/text-field';
-import '../../components/radio-button/radio-button';
-import '../../components/masked-text-field/masked-text-field';
-import '../../components/toggle/toggle';
+import { RadioButtons } from '../../components/radio-buttons/radio-buttons';
+import { MaskedTextField } from '../../components/masked-text-field/masked-text-field';
+import { Toggle } from '../../components/toggle/toggle';
 import '../../components/button/button';
 import './registration-block.sass';
+
+class RegistrationForm {
+  constructor(target) {
+    this._body = target;
+    this._radioButtons = this._getRadioButtons();
+    this._maskedField = this._getMaskedField();
+    this._toggle = this._getToggle();
+
+  }
+
+  _getRadioButtons = () => {
+    const radioButtons = this._body.querySelector('.js-radio-buttons');
+    return new RadioButtons(radioButtons);
+  }
+
+  _getMaskedField = () => {
+    const maskedField = this._body.querySelector('.js-masked-text-field');
+    return new MaskedTextField(maskedField);
+  }
+
+  _getToggle = () => {
+    const toggle = this._body.querySelector('.js-toggle')
+    return new Toggle(toggle);
+  }
+}
+
+export { RegistrationForm };
