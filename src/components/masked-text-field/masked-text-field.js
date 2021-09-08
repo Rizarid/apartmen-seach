4b6c7cbb8ca0f1c018cebeb61bab05458d22122d
Rizarid <rizarid@mail.ru>
2021-09-08 18:25:14 +0500

    Refactoring masked-text-field component

--- src/components/masked-text-field/masked-text-field.js
+++ src/components/masked-text-field/masked-text-field.js
@@ -1,11 +1,21 @@
-import * as $ from 'jquery';
 import Inputmask from 'inputmask';
 
 import '../text-field/text-field';
 import './masked-text-field.sass';
 
-{
-  const $maskedFields = $('.js-masked-text-field .text-field__field');
-  const inputmask = new Inputmask({ alias: 'datetime', inputFormat: 'dd.mm.yyyy', placeholder: 'ДД.ММ.ГГГГ' });
-  inputmask.mask($maskedFields);
+class MaskedTextField {
+  constructor(target) {
+    this._body = target;
+    this._field = this._getField();
+    this._inputmask = this._getInputmask();
+    this._inputmask.mask(this._field);
+  }
+
+  _getField = () => this._body.querySelector('.js-text-field .text-field__field');
+
+  _getInputmask = () => (
+    new Inputmask({ alias: 'datetime', inputFormat: 'dd.mm.yyyy', placeholder: 'ДД.ММ.ГГГГ' })
+  );
 }
+
+export { MaskedTextField };
