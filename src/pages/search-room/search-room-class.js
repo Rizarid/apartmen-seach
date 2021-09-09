70018e645c69d7bda0d80dcf3ff77534f0fbc064
Rizarid <rizarid@mail.ru>
2021-09-09 21:53:00 +0500

    Refactoring of search-room

--- src/pages/search-room/search-room-class.js
+++ src/pages/search-room/search-room-class.js
@@ -3,9 +3,14 @@ import { RoomCard } from '@/blocks/room-card-block/room-card';
 
 class SearchRoom {
   constructor() {
-    this._filters = new Filters();
+    this._body = this._getBody();
+    this._filters = new Filters(this._getTarget('.js-filters'));
     this._createRoomCards();
   }
+
+  _getBody = () => document.querySelector('.js-search-room');
+
+  _getTarget = (selector) => this._body.querySelector(selector);
 
  _createRoomCards = () => {
    const roomCardsSelection = Array.prototype.slice.call(document.querySelectorAll('.js-room-card-block'));
