8e60745f3c777ea22cabe0a114e7deb3eed2937f
Rizarid <rizarid@mail.ru>
2021-09-09 21:48:32 +0500

    Refactoring of card page

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -4,19 +4,27 @@ import '../../fonts/fonts.css';
 import '../../fonts/scss/_path.scss';
 import { Datepicker } from '../../components/datepicker/datepicker';
 import { SearchForm } from '../../blocks/search-block/search-block';
-import '../../blocks/registration-block/registration-block';
+import { RegistrationForm } from '../../blocks/registration-block/registration-block';
 import { Booking } from '../../blocks/booking-block/booking-block';
 import '../../blocks/sign-in-block/sign-in-block';
 import { RoomCard } from '../../blocks/room-card-block/room-card';
 import './cards.sass';
 
 {
+  const _body = document.querySelector('.js-cards');
+  const getTarget = (selector) => _body.querySelector(selector);
+
   const booking = new Booking({
-    roomPrice: 9990, discount: 2179, servicesTotal: 0, additionalServicesPrice: 300,
+    target: getTarget('.js-booking'),
+    roomPrice: 9990, 
+    discount: 2179, 
+    servicesTotal: 0, 
+    additionalServicesPrice: 300,
   });
-  const datepickerContainer = document.querySelector('.cards__date-picker-container');
-  const datepicker = new Datepicker(datepickerContainer);
-  const searchForm = new SearchForm({ dateDropdownSelector: 'date-selection', dropdownSelector: 'guests-quantity' });
+
+  const datepicker = new Datepicker(getTarget('.js-datepicker'));
+  const searchForm = new SearchForm({ target: getTarget('.js-search-block') });
+  const registration = new RegistrationForm(getTarget('.js-registration-block'));
 
   const roomCardsSelection = Array.prototype.slice.call(document.querySelectorAll('.js-room-card-block'));
   let roomCards;
