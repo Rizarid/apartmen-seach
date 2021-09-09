ff6798541e24855ff986b057962a19c897c2c3a9
Rizarid <rizarid@mail.ru>
2021-09-09 21:41:05 +0500

    Refactoring dropdown component

--- src/components/dropdown/DropdownClass.js
+++ src/components/dropdown/DropdownClass.js
@@ -1,51 +1,35 @@
-import * as $ from 'jquery';
-
 // eslint-disable-next-line no-unused-vars
-import { ListConvenience } from './__list/ListConvenience';
-import { ListGuests } from './__list/ListGuests';
+
+import { ListConvenience } from '../dropdown-list/ListConvenience';
+import { ListGuests } from '../dropdown-list/ListGuests';
 
 class DropdownClass {
   constructor(options = {}) {
-    this._init(options);
-    this._getParent(this._parentSelector);
-    this._getField();
+    const { target, getListClass = (params) => new ListGuests(params) } = options;
 
-    const list = $(this._parent).find('.dropdown__list')[0];
-    this._list = this._getListClass({ list, items: this._items });
+    this._body = target;
+    this._getField();
+    this._list = getListClass(this._getListsTarget());
 
     this._addListeners();
     this._apply();
   }
 
-  _init = (options = {}) => {
-    const {
-      parentSelector = 'dropdown',
-      items = { взрослые: 0, дети: 0, младенцы: 0 },
-      getListClass = (params) => new ListGuests(params),
-    } = options;
+  _getField = () => { this._field = this._body.querySelector('.js-dropdown__field'); };
 
-    this._parentSelector = parentSelector;
-    this._items = items;
-    this._getListClass = getListClass;
-  }
-
-  _getParent = () => {
-    this._parent = document.querySelector(`.js-${this._parentSelector}`);
-  }
-
-  _getField = () => { [this._field] = $(this._parent).find('.dropdown__field'); };
+  _getListsTarget = () => this._body.querySelector('.js-dropdown-list');
 
   _addListeners = () => {
     this._field.addEventListener('click', this._handleFieldClick);
-    this._parent.addEventListener('mouseleave', this._handleDropdownMouseleave);
+    this._body.addEventListener('mouseleave', this._handleDropdownMouseleave);
     this._list.getList().addEventListener('cleanButtonClick', this._handleCleanButtonClick);
     this._list.getList().addEventListener('applyButtonClick', this._handleApplyButtonClick);
   }
 
   _handleFieldClick = () => {
     const list = this._list.getList();
-    const isListVisible = list.classList.contains('dropdown__list_visible');
-    if (!isListVisible) list.classList.add('dropdown__list_visible');
+    const isListVisible = list.classList.contains('dropdown-list_visible');
+    if (!isListVisible) list.classList.add('dropdown-list_visible');
     else this._apply();
   }
 
@@ -60,7 +44,7 @@ class DropdownClass {
 
   _apply = () => {
     this._field.value = this._list.getListValue();
-    this._list.getList().classList.remove('dropdown__list_visible');
+    this._list.getList().classList.remove('dropdown-list_visible');
   }
 }
 
