42be5081c2fe8963951d97afce4fd6d4915436c9
Rizarid <rizarid@mail.ru>
2021-09-09 21:45:55 +0500

    Refactoring of room-card-block

--- src/blocks/room-card-block/room-card.js
+++ src/blocks/room-card-block/room-card.js
@@ -1,19 +1,18 @@
-import * as $ from 'jquery';
-
-import './room-card-block.sass';
 import '../../components/button/button';
 import '../../components/rate-button/rate-button';
-
 import { ImageSlider } from '../../components/image-slider/image-slider';
 import { numberMargins } from '../../scripts/numberMargins';
+import './room-card-block.sass';
 
 class RoomCard {
-  constructor(card) {
-    this._card = card;
-    this._slider = new ImageSlider($(this._card).find('.js-image-slider')[0]);
-    [this._price] = $(this._card).find('.js-room-card-block__price');
+  constructor(target) {
+    this._body = target;
+    this._slider = new ImageSlider(this._getTarget('.js-image-slider'));
+    this._price = this._getTarget('.js-room-card-block__price');
     this._price.innerHTML = `${numberMargins(this._price.innerHTML)}&#8381 `;
   }
+
+  _getTarget = (selector) => this._body.querySelector(selector);
 }
 
 export { RoomCard };
