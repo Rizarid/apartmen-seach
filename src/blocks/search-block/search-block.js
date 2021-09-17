cbfe5f552c326b7faaec4a95c5f690fdb4275256
Rizarid <rizarid@mail.ru>
2021-09-17 20:16:20 +0500

    Refactoring of search-block

--- src/blocks/search-block/search-block.js
+++ src/blocks/search-block/search-block.js
@@ -1,23 +1,4 @@
 import '../../components/button/button';
-import { DateDropdown } from '../../components/date-dropdown/date-dropdown';
-import { DropdownClass } from '../../components/dropdown/DropdownClass';
-import { ListGuests } from '../../components/dropdown-list/ListGuests';
+import '../../components/dropdown/dropdown-init';
+import '../../components/date-dropdown/date-dropdown-init';
 import './search-block.sass';
-
-class SearchForm {
-  constructor(options = {}) {
-    const { target, dateDropdownSelector = 'date-selection', dropdownSelector = 'guests-quantity' } = options;
-
-    this._body = target;
-    this._dateDropdown = new DateDropdown({ target: this._getTarget(dateDropdownSelector) });
-
-    this._dropdown = new DropdownClass({
-      target: this._getTarget(dropdownSelector),
-      getListClass: (items) => new ListGuests(items),
-    });
-  }
-
-  _getTarget = (selector) => this._body.querySelector(`.js-${selector}`);
-}
-
-export { SearchForm };
