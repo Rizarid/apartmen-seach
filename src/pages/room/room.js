0c5089739d06254c5683dba912763e2f01753efa
Rizarid <rizarid@mail.ru>
2021-09-19 22:51:29 +0500

    Refactoring of room page

--- src/pages/room/room.js
+++ src/pages/room/room.js
@@ -1,12 +1,8 @@
-/* eslint-disable no-unused-vars */
-
 import '../../fonts/fonts.css';
 import '../../fonts/scss/_path.scss';
-import '../../blocks/header/header';
+import '../../blocks/header/header-init';
 import '../../blocks/footer/footer';
 import '../../blocks/information-about-room/information-about-room';
+import '../../blocks/booking-block/booking-block-init';
 import '../../components/room-gallery/room-gallery';
-import { Room } from './room-class';
 import './room.sass';
-
-const room = new Room();
