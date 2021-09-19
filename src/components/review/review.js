d4193dcc2bba1b0685d8155227b7b307e1a0a218
Rizarid <rizarid@mail.ru>
2021-09-19 22:20:16 +0500

    Refactoring of review

--- src/components/review/review.js
+++ src/components/review/review.js
@@ -1,16 +1,2 @@
-import { LikeButton } from '../like-button/like-button';
+import '../like-button/like-button-init';
 import './review.sass';
-
-class Review {
-  constructor(target) {
-    this._body = target;
-    this._likeButton = this._getLikeButton();
-  }
-
-  _getLikeButton = () => {
-    const likeButtonTarget = this._body.querySelector('.js-like-button');
-    return new LikeButton(likeButtonTarget);
-  }
-}
-
-export { Review };
