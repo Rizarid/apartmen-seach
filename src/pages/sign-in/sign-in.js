6712ae45788837f98173dc3b11f6bdb6ae56ad15
Rizarid <rizarid@mail.ru>
2021-09-19 22:53:00 +0500

    Refactoring of sign-in page

--- src/pages/sign-in/sign-in.js
+++ src/pages/sign-in/sign-in.js
@@ -1,11 +1,7 @@
-/* eslint-disable no-unused-vars */
-
 import '../../fonts/fonts.css';
 import '../../fonts/scss/_path.scss';
 import '../../blocks/sign-in-block/sign-in-block';
-import '../../blocks/header/header';
+import '../../blocks/header/header-init';
 import '../../blocks/footer/footer';
+import '../../components/slider-of-background/slider-of-background-init';
 import './sign-in.sass';
-import { SignIn } from './sign-in-class';
-
-const signIn = new SignIn();
