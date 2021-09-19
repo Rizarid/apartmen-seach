88f450c1f017d48fb061367159010d0f5ef82ac2
Rizarid <rizarid@mail.ru>
2021-09-19 22:41:51 +0500

    Refactoring of information-about-room

--- src/blocks/information-about-room/information-about-room.js
+++ src/blocks/information-about-room/information-about-room.js
@@ -1,24 +1,5 @@
 import '../../components/information-block/information-block';
-import { Reviews } from '../../components/reviews/reviews';
-import { RoundChart } from '../../components/round-chart/round-chart';
+import '../../components/reviews/reviews';
+import '../../components/round-chart/round-chart-init';
 import '../../components/bullet-list/bullet-list';
 import './information-about-room.sass';
-
-class InformationAboutRoom {
-  constructor(options) {
-    const { target, votes } = options;
-    this._body = target;
-    this._reviews = new Reviews(this._getTarget('.js-reviews'));
-    this._roundChart = new RoundChart(votes);
-    this._appendChart();
-  }
-
-  _getTarget = (selector) => this._body.querySelector(selector);
-
-  _appendChart = () => {
-    const chart = this._getTarget('.js-round-chart__content');
-    chart.innerHTML = this._roundChart.getChart();
-  };
-}
-
-export { InformationAboutRoom };
