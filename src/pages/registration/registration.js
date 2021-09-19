7b5d6ccf322543f66aee067ac289d52d9c479b76
Rizarid <rizarid@mail.ru>
2021-09-19 22:50:54 +0500

    Refactoring of registration page

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -1,11 +1,7 @@
-/* eslint-disable no-unused-vars */
-
 import '../../fonts/fonts.css';
 import '../../fonts/scss/_path.scss';
 import '../../blocks/registration-block/registration-block';
-import '../../blocks/header/header';
+import '../../blocks/header/header-init';
 import '../../blocks/footer/footer';
+import '../../components/slider-of-background/slider-of-background-init';
 import './registration.sass';
-import { Registration } from './registration-class';
-
-const registration = new Registration();
